b3da36c7b412998e6e76edb7ead5829c03550c17
Mohammad Ghasembeigi <me@mohammadg.com>
2016-10-15 02:28:58 +1100

    Finish drawing terrain as triangle mesh, implement keylisterer and camera movement commands

--- src/ass2/spec/Game.java
+++ src/ass2/spec/Game.java
@@ -1,5 +1,7 @@
 package ass2.spec;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import com.jogamp.opengl.*;
@@ -16,73 +18,216 @@ import com.jogamp.opengl.util.FPSAnimator;
  *
  * @author malcolmr
  */
-public class Game extends JFrame implements GLEventListener{
-
-    private Terrain myTerrain;
-
-    public Game(Terrain terrain) {
-    	super("Assignment 2");
-        myTerrain = terrain;
-   
-    }
-    
-    /** 
-     * Run the game.
-     *
-     */
-    public void run() {
-    	  GLProfile glp = GLProfile.getDefault();
-          GLCapabilities caps = new GLCapabilities(glp);
-          GLJPanel panel = new GLJPanel();
-          panel.addGLEventListener(this);
- 
-          // Add an animator to call 'display' at 60fps        
-          FPSAnimator animator = new FPSAnimator(60);
-          animator.add(panel);
-          animator.start();
-
-          getContentPane().add(panel);
-          setSize(800, 600);        
-          setVisible(true);
-          setDefaultCloseOperation(EXIT_ON_CLOSE);        
-    }
-    
-    /**
-     * Load a level file and display it.
-     * 
-     * @param args - The first argument is a level file in JSON format
-     * @throws FileNotFoundException
-     */
-    public static void main(String[] args) throws FileNotFoundException {
-        Terrain terrain = LevelIO.load(new File(args[0]));
-        Game game = new Game(terrain);
-        game.run();
-    }
-
-	@Override
-	public void display(GLAutoDrawable drawable) {
-		GL2 gl = drawable.getGL().getGL2();
-    
-	}
+public class Game extends JFrame implements GLEventListener, KeyListener {
   
-	@Override
-	public void dispose(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
+  private Terrain myTerrain;
+  
+  //Camera
+  private int width;
+  private int height;
+  private double[] cameraPosition;
+  private double cameraRotation;
+  private GLU glu;
+  
+  //Constants
+  private static final double FIELD_OF_VIEW = 60.0; //field of view to use in world
+  private static final double ALTITUDE_OFFSET = 0.5; //camera offset from ground so world is visible
+  private static final double WALKING_SPEED = 0.1; //speed at which player (camera) moves at
+  private static final double CAMERA_ROTATION_STEP = 10; //number of degrees to rotate camera by
+  
+  
+  public Game(Terrain terrain) {
+    super("Assignment 2");
+    
+    //Default values
+    myTerrain = terrain;
+    cameraPosition = new double[]{0,0};
+    cameraRotation = 45.0;
+    width = 800;
+    height = 600;
+    glu = new GLU();
+  }
+  
+  /**
+   * Run the game.
+   *
+   */
+  public void run() {
+    GLProfile glp = GLProfile.getDefault();
+    GLCapabilities caps = new GLCapabilities(glp);
+    GLJPanel panel = new GLJPanel();
+    panel.addGLEventListener(this);
+    panel.addKeyListener(this);
+    
+    // Add an animator to call 'display' at 60fps
+    FPSAnimator animator = new FPSAnimator(60);
+    animator.add(panel);
+    animator.start();
+    
+    getContentPane().add(panel);
+    setSize(width, height);
+    setVisible(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+  
+  /**
+   * Load a level file and display it.
+   *
+   * @param args - The first argument is a level file in JSON format
+   * @throws FileNotFoundException
+   */
+  public static void main(String[] args) throws FileNotFoundException {
+    Terrain terrain = LevelIO.load(new File(args[0]));
+    Game game = new Game(terrain);
+    game.run();
+  }
+  
+  @Override
+  public void display(GLAutoDrawable drawable) {
+    GL2 gl = drawable.getGL().getGL2();
+    
+    gl.glMatrixMode(GL2.GL_MODELVIEW);
+    gl.glLoadIdentity();
+    gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+    
+    //Setup camera
+    setupCamera(gl);
+    
+    //Draw terrain
+    myTerrain.draw(gl);
+    
+    
+    
+  }
+  
+  @Override
+  public void dispose(GLAutoDrawable drawable) {
+    // TODO Auto-generated method stub
     // TODO: Probably do not need
-	}
-
-	@Override
-	public void init(GLAutoDrawable drawable) {
-		GL2 gl = drawable.getGL().getGL2();
+  }
+  
+  @Override
+  public void init(GLAutoDrawable drawable) {
+    GL2 gl = drawable.getGL().getGL2();
     
     //Background colour
-    gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //white
-	}
-
-	@Override
-	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
-			int height) {
-
+    gl.glClearColor(0.529411f, 0.807843f, 0.980392f, 1.0f); //Sky Blue, RGB: 135-206-250
     
-	}
+    //Configure required options
+    gl.glEnable(GL2.GL_DEPTH_TEST); //need Z axis taken into account
+    gl.glEnable(GL2.GL_NORMALIZE); //automatically normalise our normals as we scale
+    
+    /*
+    gl.glEnable(GL2.GL_CULL_FACE);  //optimization to get rid of various fragments
+    gl.glEnable(GL2.GL_BACK);
+    */
+    
+    //gl.glEnable(GL2.GL_LIGHTING); //enable lighting
+    //gl.glEnable(GL2.GL_LIGHT1); //skip light0 and use first standard light
+  }
+  
+  @Override
+  public void reshape(GLAutoDrawable drawable, int x, int y, int width,
+                      int height) {
+    GL2 gl = drawable.getGL().getGL2();
+    
+    gl.glMatrixMode(GL2.GL_PROJECTION);
+    gl.glLoadIdentity();
+    
+    this.width = width;
+    this.height = height;
+    double aspectRatio = width / height;
+    double fieldOfView = aspectRatio * FIELD_OF_VIEW;
+    glu.gluPerspective(fieldOfView, aspectRatio, 0.01d, 10000d);
+  }
+  
+  
+  /**
+   * Setup camera based on current window size, field of view and camera position.
+   *
+   * @param gl GL2 object
+   */
+  private void setupCamera(GL2 gl) {
+    gl.glPushMatrix();
+    
+    gl.glMatrixMode(GL2.GL_PROJECTION); //Set projection matrix mode
+    gl.glLoadIdentity();
+    
+    //We will use gluPerspective to obtain a perspective camera
+    double aspectRatio = width / height;
+    double fieldOfView = aspectRatio * FIELD_OF_VIEW;
+    
+    //set near,far so they show everything for all sample terrains of size without clipping
+    glu.gluPerspective(fieldOfView, aspectRatio, 0.01d, 10000d);
+    
+    glu.gluLookAt(//Eye is the camera location with interpolated altitude
+      cameraPosition[0],
+      myTerrain.altitude(cameraPosition[0], cameraPosition[1]) + ALTITUDE_OFFSET,
+      cameraPosition[1],
+      
+      //center of object is vector going away from camera
+      cameraPosition[0] + Math.cos(Math.toRadians(cameraRotation)),
+      myTerrain.altitude(cameraPosition[0], cameraPosition[1]) + ALTITUDE_OFFSET,
+      cameraPosition[1] + Math.sin(Math.toRadians(cameraRotation)),
+      
+      //standard camera orientation (UP)
+      0,
+      1,
+      0
+    );
+    
+    gl.glPopMatrix();
+  }
+  
+  @Override
+  public void keyTyped(KeyEvent e) {
+  }
+  
+  @Override
+  public void keyPressed(KeyEvent e) {
+    switch(e.getKeyCode()){
+      case KeyEvent.VK_UP:
+        {
+          //Calculate new camera location
+          double newX = (Math.cos(Math.toRadians(cameraRotation)) * WALKING_SPEED) + cameraPosition[0];
+          double newZ = (Math.sin(Math.toRadians(cameraRotation)) * WALKING_SPEED) + cameraPosition[1];
+          
+          //Update global camera position only if movement doesn't take us 'off the grid'
+          if (!(newX < 0 || newX > myTerrain.size().getWidth() - 1 || newZ < 0 || newZ > myTerrain.size().getHeight() - 1)) {
+            cameraPosition[0] = newX;
+            cameraPosition[1] = newZ;
+          }
+        }
+        break;
+      case KeyEvent.VK_DOWN:
+        {
+          //Calculate new camera location
+          double newX = cameraPosition[0] - (Math.cos(Math.toRadians(cameraRotation)) * WALKING_SPEED);
+          double newZ = cameraPosition[1] - (Math.sin(Math.toRadians(cameraRotation)) * WALKING_SPEED);
+    
+          //Update global camera position only if movement doesn't take us 'off the grid'
+          if (!(newX < 0 || newX > myTerrain.size().getWidth() - 1 || newZ < 0 || newZ > myTerrain.size().getHeight() - 1)) {
+            cameraPosition[0] = newX;
+            cameraPosition[1] = newZ;
+          }
+        }
+        break;
+      case KeyEvent.VK_LEFT:
+        {
+          cameraRotation -= CAMERA_ROTATION_STEP;
+        }
+        break;
+      case KeyEvent.VK_RIGHT:
+        {
+          cameraRotation += CAMERA_ROTATION_STEP;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  
+  @Override
+  public void keyReleased(KeyEvent e) {
+  }
 }
--- src/ass2/spec/Terrain.java
+++ src/ass2/spec/Terrain.java
@@ -1,5 +1,9 @@
 package ass2.spec;
 
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
 import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,144 +16,251 @@ import java.util.List;
  * @author malcolmr
  */
 public class Terrain {
-
-    private Dimension mySize;
-    private double[][] myAltitude;
-    private List<Tree> myTrees;
-    private List<Road> myRoads;
-    private float[] mySunlight;
-
-    /**
-     * Create a new terrain
-     *
-     * @param width The number of vertices in the x-direction
-     * @param depth The number of vertices in the z-direction
-     */
-    public Terrain(int width, int depth) {
-        mySize = new Dimension(width, depth);
-        myAltitude = new double[width][depth];
-        myTrees = new ArrayList<Tree>();
-        myRoads = new ArrayList<Road>();
-        mySunlight = new float[3];
-    }
+  
+  private Dimension mySize;
+  private double[][] myAltitude;
+  private List<Tree> myTrees;
+  private List<Road> myRoads;
+  private float[] mySunlight;
+  
+  /**
+   * Create a new terrain
+   *
+   * @param width The number of vertices in the x-direction
+   * @param depth The number of vertices in the z-direction
+   */
+  public Terrain(int width, int depth) {
+    mySize = new Dimension(width, depth);
+    myAltitude = new double[width][depth];
+    myTrees = new ArrayList<Tree>();
+    myRoads = new ArrayList<Road>();
+    mySunlight = new float[3];
+  }
+  
+  public Terrain(Dimension size) {
+    this(size.width, size.height);
+  }
+  
+  public Dimension size() {
+    return mySize;
+  }
+  
+  public List<Tree> trees() {
+    return myTrees;
+  }
+  
+  public List<Road> roads() {
+    return myRoads;
+  }
+  
+  public float[] getSunlight() {
+    return mySunlight;
+  }
+  
+  /**
+   * Set the sunlight direction.
+   *
+   * Note: the sun should be treated as a directional light, without a position
+   *
+   * @param dx
+   * @param dy
+   * @param dz
+   */
+  public void setSunlightDir(float dx, float dy, float dz) {
+    mySunlight[0] = dx;
+    mySunlight[1] = dy;
+    mySunlight[2] = dz;
+  }
+  
+  /**
+   * Resize the terrain, copying any old altitudes.
+   *
+   * @param width
+   * @param height
+   */
+  public void setSize(int width, int height) {
+    mySize = new Dimension(width, height);
+    double[][] oldAlt = myAltitude;
+    myAltitude = new double[width][height];
     
-    public Terrain(Dimension size) {
-        this(size.width, size.height);
+    for (int i = 0; i < width && i < oldAlt.length; i++) {
+      for (int j = 0; j < height && j < oldAlt[i].length; j++) {
+        myAltitude[i][j] = oldAlt[i][j];
+      }
     }
-
-    public Dimension size() {
-        return mySize;
-    }
-
-    public List<Tree> trees() {
-        return myTrees;
-    }
-
-    public List<Road> roads() {
-        return myRoads;
-    }
-
-    public float[] getSunlight() {
-        return mySunlight;
-    }
-
-    /**
-     * Set the sunlight direction. 
-     * 
-     * Note: the sun should be treated as a directional light, without a position
-     * 
-     * @param dx
-     * @param dy
-     * @param dz
-     */
-    public void setSunlightDir(float dx, float dy, float dz) {
-        mySunlight[0] = dx;
-        mySunlight[1] = dy;
-        mySunlight[2] = dz;        
-    }
+  }
+  
+  /**
+   * Get the altitude at a grid point
+   *
+   * @param x
+   * @param z
+   * @return
+   */
+  public double getGridAltitude(int x, int z) {
+    return myAltitude[x][z];
+  }
+  
+  /**
+   * Set the altitude at a grid point
+   *
+   * @param x
+   * @param z
+   * @return
+   */
+  public void setGridAltitude(int x, int z, double h) {
+    myAltitude[x][z] = h;
+  }
+  
+  /**
+   * Get the altitude at an arbitrary point.
+   * Non-integer points should be interpolated from neighbouring grid points
+   *
+   * TODO: Check functionality, especially on edge cases (when x is inverse etc)
+   *
+   * @param x point on x axis
+   * @param z point on z axis
+   * @return altitude at an arbitary point
+   */
+  public double altitude(double x, double z) {
+    double altitude = 0;
+  
+    //Out of bounds, return default value
+    if (x < 0 || x > mySize.width -1 || z < 0 || z > mySize.height -1 )
+      return altitude;
+  
+    //Check trivial case
+    if ((int)x == x && (int)z == z) {
+      altitude = getGridAltitude((int)x, (int)z);
+    } else {
+      //Compute closest 'grid' int coordinates
+      //We floor/ceil based on winding order (Right hand rule)
+      //Thus we know the point is enclosed in grid made by 4 points (leftX, rightX, upperZ, lowerZ).
+      double leftX = Math.floor(x);
+      double rightX = Math.ceil(x);
+      double upperZ = Math.floor(z);
+      double lowerZ = Math.ceil(z);
+      double hypotenuseX = (leftX + lowerZ) - z;
     
-    /**
-     * Resize the terrain, copying any old altitudes. 
-     * 
-     * @param width
-     * @param height
-     */
-    public void setSize(int width, int height) {
-        mySize = new Dimension(width, height);
-        double[][] oldAlt = myAltitude;
-        myAltitude = new double[width][height];
+      if ((int)x == x) {  //X provided is int
+        altitude = calcBilinearInterpolationZComponent(z, upperZ, lowerZ, x, x); //interpolate only Z component
+      } else if ((int)z == z) { //Z provided is int
+        altitude = calcBilinearInterpolationXComponent(x, leftX, rightX, z, z); //interpolate only X component
+      } else if (x < hypotenuseX) { //Point exists in left triangle, interpolate using it
+        altitude = calcBilinearInterpolation(x, leftX, leftX, rightX, z, lowerZ, upperZ, upperZ, hypotenuseX);
+      } else { //x > hypotenuseX, point exists in right triangle, interpolate using it
+        altitude = calcBilinearInterpolation(x, rightX, rightX, leftX, z, upperZ, lowerZ, lowerZ, hypotenuseX);
+      }
+    }
+  
+    return altitude;
+  }
+  
+  /**
+   * Helper bilinear interpolation function based on slides 39-41 week 5 lectures
+   */
+  private double calcBilinearInterpolationXComponent(double x, double x1, double x2, double z1, double z2) {
+    return ((x - x1) / (x2 - x1)) * getGridAltitude((int)x2, (int)z2) +
+      ((x2 - x) / (x2 - x1)) * getGridAltitude((int)x1, (int)z1);
+  }
+  
+  /**
+   * Helper bilinear interpolation function based on slides 39-41 week 5 lectures
+   */
+  private double calcBilinearInterpolationZComponent(double z, double z1, double z2, double x1, double x2) {
+    return ((z - z1) / (z2 - z1)) * getGridAltitude((int)x2, (int)z2) +
+      ((z2 - z) / (z2 - z1)) * getGridAltitude((int)x1, (int)z1);
+  }
+  
+  /**
+   * Helper bilinear interpolation function based on slides 39-41 week 5 lectures
+   */
+  private double calcBilinearInterpolation(double x, double x1, double x2, double x3,
+                                           double z, double z1, double z2, double z3,
+                                           double hypotenuseX) {
+    return ((x - x1) / (hypotenuseX - x1)) * calcBilinearInterpolationZComponent(z, z1, z3, x1, x3) +
+      ((hypotenuseX - x) / (hypotenuseX - x1)) * calcBilinearInterpolationZComponent(z, z1, z2, x1, x2);
+  }
+  
+  /**
+   * Add a tree at the specified (x,z) point.
+   * The tree's y coordinate is calculated from the altitude of the terrain at that point.
+   *
+   * @param x
+   * @param z
+   */
+  public void addTree(double x, double z) {
+    double y = altitude(x, z);
+    Tree tree = new Tree(x, y, z);
+    myTrees.add(tree);
+  }
+  
+  
+  /**
+   * Add a road.
+   *
+   * @param x
+   * @param z
+   */
+  public void addRoad(double width, double[] spine) {
+    Road road = new Road(width, spine);
+    myRoads.add(road);
+  }
+  
+  /*********************** My Code *********************/
+  
+  public void draw(GL2 gl) {
+    //System.out.println("Drawing terrain...");
+    
+    gl.glColor3d(1.0f, 0.0f, 0.0f); //TODO: remove when texuring
+    gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+    
+    //Draw the terrain one grid point at a time
+    //Each grid contains 2 triangles
+    for (int z = 0; z < mySize.height -1; ++z) {
+      for (int x = 0; x < mySize.width -1; ++x) {
+        //Fir
+        double[] v1 = {x, getGridAltitude(x, z), z};
+        double[] v2 = {x, getGridAltitude(x, z + 1), z + 1};
+        double[] v3 = {x + 1, getGridAltitude(x + 1, z), z};
         
-        for (int i = 0; i < width && i < oldAlt.length; i++) {
-            for (int j = 0; j < height && j < oldAlt[i].length; j++) {
-                myAltitude[i][j] = oldAlt[i][j];
-            }
+        double[] normal1 = MathUtil.getNormal(v1, v2, v3);
+        gl.glNormal3dv(normal1, 0);
+        
+        //Draw first triangle in grid
+        gl.glBegin(GL2.GL_TRIANGLES);
+        {
+          gl.glVertex3dv(v1, 0);
+          gl.glVertex3dv(v2, 0);
+          gl.glVertex3dv(v3, 0);
         }
+        gl.glEnd();
+  
+        double[] v4 = {x + 1, getGridAltitude(x + 1, z), z};
+        double[] v5 = {x, getGridAltitude(x, z + 1), z + 1};
+        double[] v6 = {x + 1, getGridAltitude(x + 1, z + 1), z + 1};
+  
+        double[] normal2 = MathUtil.getNormal(v4, v5, v6);
+        gl.glNormal3dv(normal2, 0);
+  
+        //Draw second triangle in grid
+        gl.glBegin(GL2.GL_TRIANGLES);
+        {
+          gl.glVertex3dv(v4, 0);
+          gl.glVertex3dv(v5, 0);
+          gl.glVertex3dv(v6, 0);
+        }
+        gl.glEnd();
+      }
     }
-
-    /**
-     * Get the altitude at a grid point
-     * 
-     * @param x
-     * @param z
-     * @return
-     */
-    public double getGridAltitude(int x, int z) {
-        return myAltitude[x][z];
-    }
-
-    /**
-     * Set the altitude at a grid point
-     * 
-     * @param x
-     * @param z
-     * @return
-     */
-    public void setGridAltitude(int x, int z, double h) {
-        myAltitude[x][z] = h;
-    }
-
-    /**
-     * Get the altitude at an arbitrary point. 
-     * Non-integer points should be interpolated from neighbouring grid points
-     * 
-     * TO BE COMPLETED
-     * 
-     * @param x
-     * @param z
-     * @return
-     */
-    public double altitude(double x, double z) {
-        double altitude = 0;
-
-        
-        
-        return altitude;
-    }
-
-    /**
-     * Add a tree at the specified (x,z) point. 
-     * The tree's y coordinate is calculated from the altitude of the terrain at that point.
-     * 
-     * @param x
-     * @param z
-     */
-    public void addTree(double x, double z) {
-        double y = altitude(x, z);
-        Tree tree = new Tree(x, y, z);
-        myTrees.add(tree);
-    }
-
-
-    /**
-     * Add a road. 
-     * 
-     * @param x
-     * @param z
-     */
-    public void addRoad(double width, double[] spine) {
-        Road road = new Road(width, spine);
-        myRoads.add(road);        
-    }
-
-
+    gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+  
+    //Draw all trees part of terrain
+    //TODO
+    
+    //Draw all roads part of terrain
+    //TODO
+  }
+  
+  
 }
